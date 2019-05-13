5b89850db50ddeed23cf091de5e3470d25e24a8b
Patience D <patience.deaton2@outlook.com>
2019-05-13 18:19:18 -0400

    file changes, running into stock quantity error

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,50 +15,79 @@ var connection = mysql.createConnection({
     database: "bamazon_db"
 });
 
-let productArry = [];
-let stockArry = [];
-let priceArry = [];
+// let productArry = [];
+// let stockArry = [];
+// let priceArry = [];
 
 connection.connect(function (err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    // start();
-});
+    if (err) {
+        console.log("error! " + err);
+        return;
 
-connection.query('SELECT * FROM products', function (error, results, fields) {
-    if (error) throw error;
-    // console.log(results);
-
-    for (var i = 0; i < results.length; i++) {
-        productArry.push(results[i].item_id + " " + results[i].product_name);
-        stockArry.push(results[i].stock_quantity);
-        priceArry.push(results[i].price);
-        // console.log(results[i].item_id + " " + results[i].product_name);
     }
-    console.log("Products: " + productArry);
-    console.log("Stock Quantities: " + stockArry);
-    console.log("Prices: " + priceArry);
+    console.log("Connection made! " + connection.threadId);
+    items();
 });
 
-connection.end();
+function items() {
+    connection.query('SELECT * FROM products', function (error, results) {
+        if (error) throw error;
+        // console.log(results);
 
-// function start() {
-//     inquirer
-//         .prompt({
-//             name: "whichproduct",
-//             type: "list",
-//             message: "Which product would you like to buy?",
-//             choices: [productArry]
-//         })
-    // .then(function(answer) {
-    //     // based on their answer, either call the bid or the post functions
-    //     if (answer.postOrBid === "POST") {
-    //       postAuction();
-    //     }
-    //     else if(answer.postOrBid === "BID") {
-    //       bidAuction();
-    //     } else{
-    //       connection.end();
-    //     }
-    //   });
-// }
+        for (var i = 0; i < results.length; i++) {
+            console.log("Item: " + results[i].item_id + " " + "$" + results[i].price + " " + results[i].product_name);
+            // console.log("Product Name: " + results[i].product_name);
+            // productArry.push(results[i].item_id + " " + results[i].product_name.split(","));
+            // stockArry.push(results[i].stock_quantity);
+            // priceArry.push(results[i].price);
+            // console.log(results[i].item_id + " " + results[i].product_name);
+        }
+        // console.log("Products: " + productArry);
+        // console.log("Stock Quantities: " + stockArry);
+        // console.log("Prices: " + priceArry);
+
+        buyItem();
+
+        // connection.end();
+    });
+};
+
+function buyItem() {
+    inquirer.prompt([{
+        name: "id",
+        type: "input",
+        message: "Which item would you like to buy? (enter id)"
+    }, {
+        name: "quantity",
+        type: "input",
+        message: "How many would you like to buy?"
+    }]).then(answer => {
+        connection.query("SELECT * FROM products", function (err, result) {
+            if (err) throw err;
+
+            var chosenItem;
+            for (var i = 0; i < result.length; i++) {
+                if (result[i].item_id === parseInt(answer.itemId)) {
+                    chosenItem = result[i];
+                }
+            }
+
+            if (chosenItem.stock_quantity > parseInt(answer.quantity)) {
+                connection.query("UPDATE products SET ? WHERE ?", [
+                    {
+                        stock_quantity: (chosenItem.stock_quantity - parseInt(answer.quantity))
+                    },
+                    {
+                        item_id: chosenItem.item_id
+                    }
+                ],
+                    function (error) {
+                        if (error) throw error;
+                        console.log("Thank you for shopping with us! Your total is " + "$" + parseInt(answer.quantity) * chosenItem.price)
+                    });
+            } else {
+                console.log("We're sorry, we don't have enough of that in stock right now.");
+            }
+        });
+    });
+};
